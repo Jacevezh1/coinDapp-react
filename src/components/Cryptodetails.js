6a89bb56688e21b6ebd48f513fe966ca7815b491
Javier <jacevesh21@gmail.com>
2022-01-28 13:16:48 -0600

    before chart

--- src/components/Cryptodetails.js
+++ src/components/Cryptodetails.js
@@ -119,7 +119,21 @@ const CryptoDetails = () => {
 				 </Title>
 				 {HTMLReactParser(cryptoDetails.description)}
 			   </Row>
-
+			   <Col className="coin-links">
+					<Title level={3} className='coin-details-heading'>
+						{cryptoDetails.name} Links
+					</Title>
+					{cryptoDetails.links.map((link) => (
+						<Row className='coin-link' key={link.name}>
+							<Title level={5} className='link-name'>
+								{link.type}
+							</Title>
+							<a href='{link.url} target="_blank" rel="namess"'>
+								{link.name}
+							</a>
+						</Row>
+					))}
+			   </Col>
 			</Col>
 		</Col>
 	  
